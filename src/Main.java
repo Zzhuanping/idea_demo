2a432534d9dbec0e5d7b6eda348c4bab049851d2
Zzhuanping <zhp1612968664@163.com>
2023-02-13 03:43:20 +0800

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(new Main().add(10,21));
-
+        otherMethod();
     }
 
     private String add(int x ,int y)
     {
         return "结果是："+x+y;
 
+    }
+
+    public static void otherMethod(){
+
+
+        System.out.println("why can i not push this");
     }
 
 }
---
+++ src/jie/Test.java
@@ -0,0 +1,7 @@
+package jie;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("i do noting");
+    }
+}
