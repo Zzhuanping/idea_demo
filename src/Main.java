972de8f15a59dd6d7a1d7799a18267dfdf3b10f7
Zzhuanping <zhp1612968664@163.com>
2023-02-13 03:39:27 +0800

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(new Main().add(10,21));
+
+    }
+
+    private String add(int x ,int y)
+    {
+        return "结果是："+x+y;
+
+    }
+
+}
